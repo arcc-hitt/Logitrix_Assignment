66ac54997ae07cfd033af3fb51f0671749b6f810
Archit Mahule <archit.mahule@mathongo.com>
2025-05-17 00:56:50 +0530

    add route setup for users and tasks

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import userRouter from './routes/user';
+import taskRouter from './routes/task';
+import swaggerSetup from './swagger';
 
 const app = express();
 app.use(express.json());
 
+// Routes
+app.use('/users', userRouter);
+app.use('/tasks', taskRouter);
+
+// Swagger UI at /api-docs
+swaggerSetup(app);
 
 export default app;
